93d29a9a327587411c00d84e4c4ddc19157e20ec
Alain <snowgoon88@gmail.com>
2016-07-11 19:55:09 +0100

    BUG first try with arrow and Handles

---
+++ Assets/Scripts/FollowingCamera.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowingCamera : MonoBehaviour {
+
+    public GameObject target;
+    private Vector3 _offset;
+
+    // Use this for initialization
+    void Start ()
+    {
+        // Initial Offset
+        _offset = transform.position - target.transform.position;
+    }
+	
+	// LastUpdate is called once per frame, after everyone updated
+	void LateUpdate ()
+    {
+        transform.position = target.transform.position + _offset;
+	}
+}
---
+++ Assets/Scripts/ForceCtrl.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForceCtrl : MonoBehaviour {
+
+    Rigidbody _rbody;
+
+	// Use this for initialization
+	void Start () {
+        _rbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    // Physics Update
+    void FixedUpdate()
+    {
+        float h_move = Input.GetAxis( "Horizontal" );
+        float v_move = Input.GetAxis( "Vertical" );
+
+        Vector3 move = new Vector3( h_move, 0, v_move );
+        _rbody.AddForce( move, ForceMode.Force );
+        if( move.magnitude > 0 ) {
+            Debug.Log( Time.realtimeSinceStartup+"=>Apply force "+move );
+        }
+
+        UnityEditor.Handles.color = Color.white;
+        UnityEditor.Handles.ArrowCap( 0, _rbody.position, 
+            Quaternion.LookRotation(_rbody.velocity), 
+            _rbody.velocity.magnitude);
+    }
+}
