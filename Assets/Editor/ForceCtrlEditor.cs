7c5d1b7970e7368b38480296ba21357d2a87097f
Alain <snowgoon88@gmail.com>
2016-07-15 14:29:56 +0100

    ADD desiredVelocity, rotation to velocity

--- Assets/Editor/ForceCtrlEditor.cs
+++ Assets/Editor/ForceCtrlEditor.cs
@@ -6,22 +6,35 @@ using UnityEngine.Assertions;
 [CustomEditor(typeof(ForceCtrl))]
 public class ForceCtrlEditor : Editor {
 
+    ForceCtrl _myCtrl;
     private Rigidbody _rbody;
-
+    
     void OnEnable()
     {
         Debug.Log( "ForceCtrlEditor" );
-        ForceCtrl myCtrl = target as ForceCtrl;
-        _rbody = myCtrl.GetComponent<Rigidbody>();
+        _myCtrl = target as ForceCtrl;
+        _rbody = _myCtrl.GetComponent<Rigidbody>();
         Assert.IsNotNull( _rbody );
     }
 
 	void OnSceneGUI()
     {
-        UnityEditor.Handles.color = Color.white;
-        UnityEditor.Handles.ArrowCap( 0, _rbody.position,
-            Quaternion.LookRotation( _rbody.velocity ),
-            _rbody.velocity.magnitude*10 );
+        // velocity
+        if( _rbody.velocity.magnitude > 0.1 ) {
+            Handles.color = Color.white;
+            Handles.ArrowCap( 0, _rbody.position,
+                Quaternion.LookRotation( _rbody.velocity ),
+                _rbody.velocity.magnitude );
+        }
+        // desired velocity
+        Vector3 _desiredVelocity = _myCtrl.DesiredVelocity;
+        if( _desiredVelocity.magnitude > 0.1 ) {
+            Handles.color = Color.red;
+            Handles.ArrowCap( 0, _rbody.position,
+                Quaternion.LookRotation( _desiredVelocity ),
+                _desiredVelocity.magnitude );
+        }
+
         //Debug.Log( Time.realtimeSinceStartup+"=>velocity "+_rbody.velocity );
     }
 }
--- Assets/Scripts/ForceCtrl.cs
+++ Assets/Scripts/ForceCtrl.cs
@@ -3,17 +3,32 @@ using System.Collections;
 
 public class ForceCtrl : MonoBehaviour {
 
-    Rigidbody _rbody;
+    public Transform cible;
 
-	// Use this for initialization
-	void Start () {
+    Rigidbody _rbody;
+    float _maxSpeed = 2.0f;
+    float _maxForce = 10.0f;
+    Vector3 _desiredVelocity;
+
+    public Vector3 DesiredVelocity
+    {
+        get
+        {
+            return _desiredVelocity;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
         _rbody = GetComponent<Rigidbody>();
+        _rbody.interpolation = RigidbodyInterpolation.Interpolate;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        Quaternion newRotation = Quaternion.LookRotation( _rbody.velocity);
+        _rbody.MoveRotation( newRotation );
+    }
 
     // Physics Update
     void FixedUpdate()
@@ -22,11 +37,27 @@ public class ForceCtrl : MonoBehaviour {
         float v_move = Input.GetAxis( "Vertical" );
 
         Vector3 move = new Vector3( h_move, 0, v_move );
-        _rbody.AddForce( move, ForceMode.Force );
+        Vector3 desiredForce = move + forceSeek();
+        float intensity = Mathf.Max( _maxForce, desiredForce.magnitude );
+        desiredForce = desiredForce.normalized * intensity;
+
+        _rbody.AddForce( desiredForce, ForceMode.Force );
         //if( move.magnitude > 0 ) {
         //    Debug.Log( Time.realtimeSinceStartup+"=>Apply force "+move );
         //}
 
         
+    }
+
+    /**
+     * Steering force to a target
+     */
+    Vector3 forceSeek()
+    {
+        // Max velocity to target
+        _desiredVelocity = ( cible.position - _rbody.transform.position ).normalized * _maxSpeed;
+        Vector3 force = _desiredVelocity - _rbody.velocity;
+
+        return force;
     }
 }
